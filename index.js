ab5461fb791cce3fe2abd1655a0eac94d9b25c69
ihsansatriawan <ihsan.satriawan.20@gmail.com>
2018-05-26 14:16:40 +0700

    Setup redux

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ import ReactDOM from 'react-dom';
 
 // main app
 import App from './containers/App';
+import { Provider } from 'react-redux';
+import store from './store';
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+)
